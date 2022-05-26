03547cf3cdc858dc98708d7f349eb067394fe2cd
Anton <antonshumilin82@gmail.com>
2022-05-26 13:01:10 +0300

    Исправил задание в Уроке 9, теперь всё работает правильно

--- Lesson 9/script.js
+++ Lesson 9/script.js
@@ -1,9 +1,16 @@
 'use strict'
 
 let age = document.getElementById('age');
- 
-function showUser(surname, name) {
-         alert("Пользователь " + surname + " " + name + ", его возраст " + age.value);
-}
- 
-showUser('Shumilin', 'Anton');
+
+function showUser(surname, name = 'Ivan') {
+    alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+    console.log(`Пользователь ${surname} ${name}, его возраст ${this.value})`);
+};
+
+showUser.call(age, 'Shumilin', 'Anton');
+showUser.apply(age, ['Shumilin', 'Anton']);
+
+let user = showUser.bind(age);
+
+user('Shumilin');
+
